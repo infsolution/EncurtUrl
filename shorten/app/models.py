f14cec1283bb5f1c225d380d95ef0890305ae863
infsolution <clsinfsolution@gmail.com>
2019-04-17 22:17:50 -0300

    Criada class Shortened

--- shorten/app/models.py
+++ shorten/app/models.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.models import User
 from django.db import models
+from random import randint
 
-# Create your models here.
+class Shortened(models.Model):
+	user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE, null=True)
+	url = models.CharField(max_length=1024)
+	url_shortened = models.CharField(max_length=256, null=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	expiring_date = models.DateTimeField(auto_now_add=False, null=True)
+	code = models.CharField(max_length=12, null=True)
+
+	def shorten(self):
+		self.url_shortened = self.get_code()
+
+
+	def get_code(self):
+		code = ''
+		for num in range(0,6):
+			char = randint(1,4)
+			if char == 1:
+				code += str(chr(randint(48,57)))
+			elif char == 2:
+				code += str(chr(randint(65,90)))
+			else:
+				code += str(chr(randint(97,122)))
+		return code
+
--- shorten/app/views.py
+++ shorten/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import *
+def index(request):
+    return render(request,'app/index.html')
 
-# Create your views here.
+def shorten(request):
+	if request.GET.get('url'):
+		short = Shortened(url=request.GET.get('url'))
+		short.shorten()
+		return render(request, 'app/index.html',{"url_short":short.url_shortened})
